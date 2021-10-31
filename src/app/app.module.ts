e9fe58ca87d67ff708c335c022704514b5bda616
jludovissie <jludovissie24@gmail.com>
2021-10-31 16:18:51 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+    imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
+    declarations: [ 
+                  AppComponent, 
+                  HelloComponent, 
+                  NavbarComponent,   
+                  FormComponent ],
+
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
